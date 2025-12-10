23940f3fd5ccc512088b02e44dc1855dd26c55be
Ahmed-kindacool <ahmedali.02khan@gmail.com>
2025-12-10 13:35:54 +0500

    Added basic functions to get minimum and maximum

--- src/ahmed.py
+++ src/ahmed.py
@@ -1,12 +1,22 @@
 #Ahmed's working file :)
 import numpy as np
+import pandas as pd
 
 class NumPyfunc:
     def __init__(self, d):
         self.d = d
         self.a = d.select_dtypes(include=[np.number]).to_numpy()
+    
     def get_mean(self):
-        return np.mean(self.a, axis=0)     # axis 0 is Row , this function gets the mean
+        return np.mean(self.a, axis=0)     
+    
+    def get_min(self):
+        return np.min(self.a, axis=0)     
+    
+    def get_max(self):
+        return np.max(self.a, axis=0)
+    
+    
     
     
         
