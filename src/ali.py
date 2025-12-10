4418341d3bf14d3a98111e2a436795ac1c6223ee
AliZafar1215 <alizafarems@gamil.com>
2025-12-10 13:28:02 +0500

    initialize pandas processor class

--- src/ali.py
+++ src/ali.py
@@ -1,1 +1,7 @@
-#Ali's working file
+#Ali's working filei
+import pandas as pd
+import numpy as np
+class PandasProcessor:
+    
+    def __init__(self, file_path):
+        self.df = pd.read_csv(file_path)
