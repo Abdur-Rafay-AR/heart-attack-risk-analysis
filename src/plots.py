18fb80aab46d92cbe8b50ff85d0cbfe6e22c62dc
AliZafar1215 <alizafarems@gamil.com>
2025-12-11 12:55:47 +0500

    Add LinePlotter class with simple line graph functionality

--- src/plots.py
+++ src/plots.py
@@ -1,1 +1,14 @@
 #file for plotting using matplotlib
+
+import matplotlib.pyplot as plt
+
+class LinePlotter:
+    def __init__(self, df):
+        self.df = df
+
+    def line_plot(self, column):
+        plt.plot(self.df[column])
+        plt.title(f"{column} Line Plot")
+        plt.xlabel("Index")
+        plt.ylabel(column)
+        plt.show()
