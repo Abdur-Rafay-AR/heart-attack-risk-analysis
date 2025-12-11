cf6d9549f4f83305a29a98a575573190877d5687
AliZafar1215 <alizafarems@gamil.com>
2025-12-11 12:57:27 +0500

    Add ScatterPlotter class with x–y scatter plot functionality

--- src/plots.py
+++ src/plots.py
@@ -12,3 +12,14 @@ class LinePlotter:
         plt.xlabel("Index")
         plt.ylabel(column)
         plt.show()
+    
+class ScatterPlotter:
+    def __init__(self, df):
+        self.df = df
+
+    def scatter_plot(self, x_col, y_col):
+        plt.scatter(self.df[x_col], self.df[y_col])
+        plt.title(f"{x_col} vs {y_col} Scatter Plot")
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+        plt.show()
